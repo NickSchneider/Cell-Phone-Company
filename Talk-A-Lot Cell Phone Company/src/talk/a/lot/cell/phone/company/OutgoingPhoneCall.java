df21bb9e1c77ced18c0e93f3240372288a0a96ad
ns0913569 <ns0913569@DT039986.otc.edu>
2018-11-01 11:56:39 -0500

    added Subclasses for types of call

---
+++ Talk-A-Lot Cell Phone Company/src/talk/a/lot/cell/phone/company/OutgoingPhoneCall.java
@@ -0,0 +1,36 @@
+package talk.a.lot.cell.phone.company;
+
+public class OutgoingPhoneCall extends PhoneCall 
+{
+    double time;
+    double totalPrice;
+    OutgoingPhoneCall(String phoneNum, double time)
+    {
+        super(phoneNum);
+    }
+    @Override
+    public String getPhoneNumber()
+    {
+        return phoneNum;
+    }
+    @Override
+    public void setPrice()
+    {
+        price = 0.04;
+    }  
+
+    @Override
+    public double getCallPrice()
+    {
+        return totalPrice = price * time;
+    }
+    
+    @Override
+    public void displayCallInfo()
+    {
+        System.out.println("Phone Number: " + getPhoneNumber());
+        System.out.println("Rate per minute: " + price);
+        System.out.println("Call time: " + time + " minutes");
+        System.out.println("Price of call: " + getCallPrice());
+    }
+}
